c028b49c4118e11c1bb9d35a68dbaf542334f285
yiyan wang <13171209963@163.com>
2022-08-15 22:59:30 +1000

    first update

---
+++ reactapp/src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+import {BrowserRouter, Routes, Route} from "react-router-dom";
+import {Main} from "./pages/Main";
+
+function App() {
+  return (
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Main/>} />
+    </Routes>
+  </BrowserRouter>
+  );
+}
+
+export default App;
