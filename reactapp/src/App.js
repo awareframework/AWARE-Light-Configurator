2081631ae83b6a9d1f1189f201057b3b7787b9db
Tianyi Zhang <tianyiz4@4180L-206821-M.local>
2022-08-20 02:42:18 +1000

    create sidebar

--- reactapp/src/App.js
+++ reactapp/src/App.js
@@ -2,12 +2,15 @@ import logo from './logo.svg';
 import './App.css';
 import {BrowserRouter, Routes, Route} from "react-router-dom";
 import {Main} from "./pages/Main";
+import {StudyInformation} from "./pages/StudyInformation";
+import Sidebar from "./components/sidebar/Sidebar";
 
 function App() {
   return (
   <BrowserRouter>
     <Routes>
-      <Route path="/" element={<Main/>} />
+      {/*<Route path="/" element={<Main/>} />*/}
+      <Route path="/" element={<Sidebar/>} />
     </Routes>
   </BrowserRouter>
   );
---
+++ reactapp/src/pages/StudyInformation.jsx
@@ -0,0 +1,7 @@
+import './StudyInformation.css'
+import {Outlet, Route, Routes} from "react-router-dom";
+import Sidebar from "../components/sidebar/Sidebar";
+
+export const StudyInformation = () => {
+
+}
