17050592ae970fa81c1f1643a34f7429bda80a4a
yiyan wang <13171209963@163.com>
2022-11-24 12:35:29 +1100

    add preparation step before deployment.

---
+++ preparation.sh
@@ -0,0 +1,39 @@
+#! /bin/bash
+
+CUR_PATH=$(pwd)
+
+
+REPLACEABLE_IP_ADDR="localhost"
+REPLACEABLE_PORT_NUM="8000"
+REPLACEABLE_STATIC_ROOT="$(echo "$CUR_PATH" | sed 's/\//\\\//g')\/static"
+echo $REPLACEABLE_STATIC_ROOT
+
+replace_parameter(){
+    files=$(ls "$1")
+    for file in $files
+    do
+        if test -d "$1/$file"
+        then
+            if [ "$file" != "node_modules" ]
+            then
+              replace_parameter "$1/$file"
+            fi
+        else
+            if [ "${file##*.}"x = "py"x ]||[ "${file##*.}"x = "js"x ]
+            then
+                sed -i "s/\[REPLACEABLE_IP_ADDR\]/$REPLACEABLE_IP_ADDR/" "$1/$file"
+                sed -i "s/\[REPLACEABLE_PORT_NUM\]/$REPLACEABLE_PORT_NUM/" "$1/$file"
+                sed -i "s/\[REPLACEABLE_STATIC_ROOT\]/$REPLACEABLE_STATIC_ROOT/" "$1/$file"
+            fi
+        fi
+    done
+}
+
+replace_parameter "$CUR_PATH"
+
+cd "$CUR_PATH/reactapp" || exit
+npm install
+npm run build
+
+cd "$CUR_PATH" || exit
+echo yes | python manage.py collectstatic
