b2aee8719522acb095d12967586ed7dde47dbc92
yiyan wang <13171209963@163.com>
2022-12-15 11:27:15 +1100

    removed the popup window and put it behind the final button with red color

--- reactapp/src/pages/Overview.jsx
+++ reactapp/src/pages/Overview.jsx
@@ -1003,6 +1003,15 @@ export default function Main() {
               columnSpacing={{ xs: 1, sm: 2, md: 23 }}
             >
               <Grid xs />
+              <Grid>
+                <p style={{ color: "#FF0000", fontWeight: "bold" }}>Note:</p>
+                <p style={{ color: "#FF0000" }}>
+                  So that we can compile stats on what sensors people are using,
+                  we will be storing the details of your configuration file
+                  anonymously minus your database credentials once you generate
+                  the file.
+                </p>
+              </Grid>
               <Grid xs="auto">
                 <Button
                   color="main"
